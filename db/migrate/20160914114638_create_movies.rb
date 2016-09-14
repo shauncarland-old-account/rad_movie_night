745d4492c64f8dc6f1058a1bd5c7f478381dd4ff
Shaun Carland <shauncarland@Shauns-MacBook-Pro.local>
2016-09-14 09:32:19 -0400

    Fix syntax error in migration.  Run migration.

--- db/migrate/20160914114638_create_movies.rb
+++ db/migrate/20160914114638_create_movies.rb
@@ -3,7 +3,8 @@ class CreateMovies < ActiveRecord::Migration[5.0]
     create_table :movies do |t|
       t.string :name, null: false
       t.integer :year
-      t.description
+      t.string :description
+
       t.timestamps
     end
   end

796604199f2b8d3ed927611c842e69d986485620
Shaun Carland <shauncarland@Shauns-MacBook-Pro.local>
2016-09-14 10:16:22 -0400

    Add poll_options routing resource.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :polls
+  resources :poll_options
   root 'polls#index'
 
 end

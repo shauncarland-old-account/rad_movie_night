c74cd2fffa54efd83ac0b952428ba070f0cc5ab9
Shaun Carland <shaun@Shauns-MacBook-Pro.local>
2016-11-11 14:04:45 -0500

    Create poll factory.  Start workign on poll option spec where the poll option doesnt belong to a poll.

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,12 @@ FactoryGirl.define do
     sequence(:name) { |n| "Example title #{n}" }
     year 2006
   end
+
+  factory :poll do
+    month { Date::MONTHNAMES[Date.today.month] }
+    year { Date.today.year }
+  end
+
+  factory :poll_option do
+  end
 end
--- spec/models/poll_option_spec.rb
+++ spec/models/poll_option_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 
 describe PollOption do
   describe 'when the poll option does not belong to a poll' do
-    # let!(:poll_option) { FactoryGirl.create(:poll_option) }
-
     it 'does not create a valid poll option' do
-      expect(PollOption.new).to have(1).error_on(:poll_id)
+      expect(PollOption.new).to be_invalid
     end
+  end
+
+  describe 'when the poll option belongs to a poll' do
+    let!(:poll) { FactoryGirl.create(:poll) }
   end
 end

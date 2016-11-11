ab71d746738ef249c2c00affbfe14670352c0737
Shaun Carland <shaun@Shauns-MacBook-Pro.local>
2016-11-11 14:14:01 -0500

    Add spec for poll_options method on polls.

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -8,4 +8,18 @@ describe Poll do
       expect(poll).to be_valid
     end
   end
+
+  describe 'when there is a poll' do
+    let!(:poll) { FactoryGirl.create(:poll) }
+    describe 'and the poll has three poll options' do
+      let!(:poll_option_1) { FactoryGirl.create(:poll_option, poll: poll) }
+      let!(:poll_option_2) { FactoryGirl.create(:poll_option, poll: poll) }
+      let!(:poll_option_3) { FactoryGirl.create(:poll_option, poll: poll) }
+
+      it 'returns an array with the three poll options when .poll_options method is called' do
+        expect(poll.poll_options.count).to eq(3)
+        expect(poll.poll_options).to eq([poll_option_1,poll_option_2,poll_option_3])
+      end
+    end
+  end
 end

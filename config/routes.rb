825d0ae1e08e02ff317dbdc2b05f2a42ab40f833
Shaun Carland <shaun@Shauns-MacBook-Pro.local>
2016-11-12 10:09:54 -0500

    Revert "Add welcome controller with welcome (home) index."
    This reverts commit 154b48dc8f2a9b9cdb9e6ef531feb6b0b517c962.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :polls
   resources :poll_options
-  root 'welcome#index'
+  root 'polls#index'
 end

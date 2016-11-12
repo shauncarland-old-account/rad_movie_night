154b48dc8f2a9b9cdb9e6ef531feb6b0b517c962
Shaun Carland <shaun@Shauns-MacBook-Pro.local>
2016-11-12 10:08:02 -0500

    Add welcome controller with welcome (home) index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
   resources :polls
   resources :poll_options
-  root 'polls#index'
+  root 'welcome#index'
 end
